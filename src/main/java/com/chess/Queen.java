7deae008f24546a34b1dabe83c38c0dc45cecd35
chaitralipalasdeokarc <chaitrali.p@thoughtworks.com>
2024-10-30 10:19:31 +0530

    Add logic for returning all possible moves for queen

---
+++ src/main/java/com/chess/Queen.java
@@ -0,0 +1,30 @@
+package com.chess;
+
+import com.chess.utils.PositionUtils;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Queen extends Piece{
+
+  public Queen(String position) {
+    super(position);
+  }
+
+  @Override
+  public List<String> allPossibleMoves() {
+    List<String> moves = new ArrayList<>();
+    int[][] directions = { {1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1} };
+
+    for (int[] dir : directions) {
+      int newRow = row;
+      int newCol = column;
+      while (true) {
+        newRow += dir[0];
+        newCol += dir[1];
+        if (!isWithinBoard(newRow, newCol)) break;
+        moves.add(PositionUtils.formatPosition(newRow, newCol));
+      }
+    }
+    return moves;
+  }
+}
---
+++ src/test/java/com/chess/QueenTest.java
@@ -0,0 +1,35 @@
+package com.chess;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class QueenTest {
+
+  @Test
+  void shouldReturnAllPossibleMoves() {
+    Queen queen = new Queen("E4");
+    final List<String> expectedMoves = List.of("A4", "B4", "C4","D4","F4","G4","H4","E1","E2","E3","E5","E6","E7","E8","A8","B7","C6","D5","F3","G2","H1","B1","C2","D3","F5","G6","H7");
+
+    final List<String> moves = queen.allPossibleMoves();
+
+    assertEquals(27, moves.size(), "Queen should have 27 possible moves from E4");
+    assertTrue(moves.containsAll(expectedMoves));
+  }
+
+  @Test
+  void shouldReturnAllPossibleMovesFromCorner() {
+    Queen queen = new Queen("A1");
+    final List<String> expectedMoves = List.of(
+        "A2", "A3", "A4", "A5", "A6", "A7", "A8",  // vertical up
+        "B1", "C1", "D1", "E1", "F1", "G1", "H1",  // horizontal right
+        "B2", "C3", "D4", "E5", "F6", "G7", "H8"   // diagonal up-right
+    );
+
+    List<String> actualMoves = queen.allPossibleMoves();
+
+    assertTrue(actualMoves.containsAll(expectedMoves));
+  }
+}
