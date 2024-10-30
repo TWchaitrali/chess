570c42505465b31038bb8503cb7f66881a41b735
chaitralipalasdeokarc <chaitrali.p@thoughtworks.com>
2024-10-30 11:20:45 +0530

    Create utility class for exception messages

---
+++ src/main/java/com/chess/utils/ExceptionMessages.java
@@ -0,0 +1,8 @@
+package com.chess.utils;
+
+public class ExceptionMessages {
+  public static final String INVALID_POSITION_FORMAT = "Invalid position format:It must exactly contain 2 characters";
+  public static final String INVALID_POSITION_FIRST_CHAR = "Invalid position format: The first character must be a letter (A-H).";
+  public static final String INVALID_POSITION_SECOND_CHAR = "Invalid position format: The second character must be a digit (1-8).";
+
+}
