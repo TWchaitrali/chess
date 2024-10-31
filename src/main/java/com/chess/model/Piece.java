03b5fc16835b1c73f195d14615e95766eb25f631
chaitralipalasdeokarc <chaitrali.p@thoughtworks.com>
2024-10-31 11:28:47 +0530

    Refactor: remove input validation logic from chess piece

--- src/main/java/com/chess/model/Piece.java
+++ src/main/java/com/chess/model/Piece.java
@@ -1,11 +1,5 @@
 package com.chess.model;
 
-import static com.chess.utils.ExceptionMessages.INVALID_POSITION_FIRST_CHAR;
-import static com.chess.utils.ExceptionMessages.INVALID_POSITION_FORMAT;
-import static com.chess.utils.ExceptionMessages.INVALID_POSITION_SECOND_CHAR;
-
-import com.chess.exception.ValidationException;
-import com.chess.utils.PositionUtils;
 import java.util.List;
 
 public abstract class Piece {
@@ -17,25 +11,9 @@ public abstract class Piece {
 
 
   protected Piece(Position position) {
-    validate(PositionUtils.formatPosition(position.row(), position.column()));
     this.position = position;
     this.row = position.row();
     this.column = position.column();
-  }
-
-  private void validate(String position) {
-    if (position.length() != 2) {
-      throw new ValidationException(INVALID_POSITION_FORMAT);
-    }
-    char columnChar = position.charAt(0);
-    char rowChar = position.charAt(1);
-
-    if (!Character.isLetter(columnChar)) {
-      throw new ValidationException(INVALID_POSITION_FIRST_CHAR);
-    }
-    if(!Character.isDigit(rowChar)){
-      throw new ValidationException(INVALID_POSITION_SECOND_CHAR);
-    }
   }
 
   public abstract List<String> allPossibleMoves();
