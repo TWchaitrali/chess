47007838a231744b0e4c9490ff9e75870fdf6546
chaitralipalasdeokarc <chaitrali.p@thoughtworks.com>
2024-10-31 13:06:10 +0530

    Clean up: remove unused classes and methods

--- src/main/java/com/chess/model/Piece.java
+++ src/main/java/com/chess/model/Piece.java
@@ -4,23 +4,12 @@ import java.util.List;
 
 public abstract class Piece {
 
-  protected int row;
-  protected int column;
-  private static final int BOARD_SIZE = 8;
   Position position;
 
 
   protected Piece(Position position) {
     this.position = position;
-    this.row = position.row();
-    this.column = position.column();
   }
 
   public abstract List<Position> allPossibleMoves();
-
-  protected boolean isWithinBoard(int newRow, int newColumn) {
-    boolean isWithinRowBounds = newRow >= 1 && newRow <= BOARD_SIZE;
-    boolean isWithinColumnBounds = newColumn >= 1 && newColumn <= BOARD_SIZE;
-    return isWithinRowBounds && isWithinColumnBounds;
-  }
 }
