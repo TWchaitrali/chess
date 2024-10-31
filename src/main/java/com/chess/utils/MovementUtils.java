99f58e9161ea5d8e2135ac44fe681737609577b9
chaitralipalasdeokarc <chaitrali.p@thoughtworks.com>
2024-10-31 12:32:51 +0530

    Refactor: Add movement utils class to move common logic in a single class

---
+++ src/main/java/com/chess/utils/MovementUtils.java
@@ -0,0 +1,30 @@
+package com.chess.utils;
+
+import com.chess.model.Position;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class MovementUtils {
+
+  public static List<Position> getMoves(Position position, List<Direction> directions, int steps) {
+    return directions.stream()
+        .map(direction -> calculateMovesInDirection(position, steps, direction))
+        .flatMap(Collection::stream)
+        .toList();
+  }
+
+  private static List<Position> calculateMovesInDirection(Position position, int steps,
+      Direction direction) {
+
+    return IntStream.range(1, steps + 1)
+        .mapToObj(step -> getNextPosition(position, direction, step))
+        .filter(Position::isWithinBoard).toList();
+  }
+
+  private static Position getNextPosition(Position position, Direction direction, int step) {
+    final int nextRow = position.row() + direction.getRowChange() * step;
+    final int nextCol = position.column() + direction.getColChange() * step;
+    return new Position(nextRow, nextCol);
+  }
+}
---
+++ src/test/java/com/chess/utils/MovementUtilsTest.java
@@ -0,0 +1,73 @@
+package com.chess.utils;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.chess.model.Position;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class MovementUtilsTest {
+
+  @Test
+  void shouldReturnNextStepsForForwardGivenPositionAndSteps() {
+    final List<Position> moves = MovementUtils.getMoves(new Position(4, 4),
+        List.of(Direction.FORWARD), 1);
+
+    assertEquals(1, moves.size());
+    assertEquals(moves, List.of(new Position(5, 4)));
+  }
+
+  @Test
+  void shouldReturnNextStepsForBackwardGivenPositionAndSteps() {
+    final List<Position> moves = MovementUtils.getMoves(new Position(4, 4),
+        List.of(Direction.BACKWARD), 1);
+
+    assertEquals(1, moves.size());
+    assertEquals(List.of(new Position(3, 4)), moves);
+  }
+
+  @Test
+  void shouldReturnNextStepsForLeftGivenPositionAndSteps() {
+    final List<Position> moves = MovementUtils.getMoves(new Position(4, 4),
+        List.of(Direction.LEFT), 1);
+
+    assertEquals(1, moves.size());
+    assertEquals(List.of(new Position(4, 3)), moves);
+  }
+
+  @Test
+  void shouldReturnNextStepsForRightGivenPositionAndSteps() {
+    final List<Position> moves = MovementUtils.getMoves(new Position(4, 4),
+        List.of(Direction.RIGHT), 1);
+
+    assertEquals(1, moves.size());
+    assertEquals(List.of(new Position(4, 5)), moves);
+  }
+
+  @Test
+  void shouldReturnNextStepsForForwardRightGivenPositionAndSteps() {
+    final List<Position> moves = MovementUtils.getMoves(new Position(4, 4),
+        List.of(Direction.FORWARD_RIGHT), 1);
+
+    assertEquals(1, moves.size());
+    assertEquals(List.of(new Position(5, 5)), moves);
+  }
+
+  @Test
+  void shouldReturnNextStepsForBackWardRightGivenPositionAndSteps() {
+    final List<Position> moves = MovementUtils.getMoves(new Position(4, 4),
+        List.of(Direction.BACKWARD_RIGHT), 1);
+
+    assertEquals(1, moves.size());
+    assertEquals(List.of(new Position(3, 5)), moves);
+  }
+
+  @Test
+  void shouldReturnNextStepsForBackWardLeftGivenPositionAndSteps() {
+    final List<Position> moves = MovementUtils.getMoves(new Position(4, 4),
+        List.of(Direction.BACKWARD_LEFT), 1);
+
+    assertEquals(1, moves.size());
+    assertEquals(List.of(new Position(3, 3)), moves);
+  }
+}
